13ff6ee4fe612538b3af70fd23f7aa34d9fdc256
Caio <caio.balassoni1510@gmail.com>
2026-03-29 21:43:31 -0300

    problemas serios arrumados sobre a responsividade(quem que fez essa desgraca?)

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,35 @@
-// define os elementos
-barraUser = document.getElementById("barra-user");
-mostra = document.getElementById("barra-user").classList;
+// // define os elementos
+// barraUser = document.getElementById("barra-user");
+// mostra = document.getElementById("barra-user").classList;
 
-// controla a visiblidade da barra do usuário
-function visibilidadeBarraUser() {
-    if (mostra == "hide") {
-        barraUser.classList.remove("hide");
-        console.log("tu clicou");
-    }
-    else {
-        barraUser.classList.add("hide");
-        console.log("tu clicou");
-    }
+// // controla a visiblidade da barra do usuário
+// function visibilidadeBarraUser() {
+//     if (mostra == "hide") {
+//         barraUser.classList.remove("hide");
+//         console.log("tu clicou");
+//     }
+//     else {
+//         barraUser.classList.add("hide");
+//         console.log("tu clicou");
+//     }
 
-}
+// }
 
-// esses cabas abaixo é para definir quem fecha e abre o bglh da barra do user
+// // esses cabas abaixo é para definir quem fecha e abre o bglh da barra do user
 
-fotoPerfil = document.querySelector(".foto-perfil");
-fotoPerfil.addEventListener('click', visibilidadeBarraUser);
+// fotoPerfil = document.querySelector(".foto-perfil");
+// fotoPerfil.addEventListener('click', visibilidadeBarraUser);
 
-btnFechar = document.getElementById("btn-fechar");
-btnFechar.addEventListener('click', visibilidadeBarraUser)
+// btnFechar = document.getElementById("btn-fechar");
+// btnFechar.addEventListener('click', visibilidadeBarraUser)
 
-// abaixo segue o código para mudar a cor do site
-function mudarCorSite() {
-    CorTema = document.getElementById("input-tema").value;
-    CorBG = document.getElementById("input-bg").value;
-    document.documentElement.style.setProperty('--CorTema', CorTema);
-    document.documentElement.style.setProperty('--CorFundo', CorBG);
-}
+// // abaixo segue o código para mudar a cor do site
+// function mudarCorSite() {
+//     CorTema = document.getElementById("input-tema").value;
+//     CorBG = document.getElementById("input-bg").value;
+//     document.documentElement.style.setProperty('--CorTema', CorTema);
+//     document.documentElement.style.setProperty('--CorFundo', CorBG);
+// }
 
-btnMudarCor = document.getElementById("btn-mudar-cor");
-btnMudarCor.addEventListener('click', mudarCorSite);
+// btnMudarCor = document.getElementById("btn-mudar-cor");
+// btnMudarCor.addEventListener('click', mudarCorSite);
