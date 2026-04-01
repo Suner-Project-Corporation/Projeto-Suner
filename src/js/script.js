f1de0805148542dd2a64cd1ca49b8a858381041d
Yann <yannpietroc@gmail.com>
2026-03-31 22:32:56 -0300

    Adicionando algumas funcionalidades

--- src/js/script.js
+++ src/js/script.js
@@ -35,7 +35,6 @@
 // btnMudarCor.addEventListener('click', mudarCorSite);
 
 //Código do banner:
-
   let imagens = ["./src/assets/image/banner/banner_epico.jfif", "./src/assets/image/banner/mikey.jpg"];
   let index = 0;
 
@@ -44,4 +43,67 @@
     document.getElementById("banner-bemvindo").src = imagens[index];
   }, 3000)
 
-  
+// Musicas
+let musicas = [];
+let indexmusica = 0;
+let musica = document.getElementById("musica")
+
+fetch("./musicas.json")
+  .then(res => res.json())
+  .then(data => {
+    // transformar objeto em lista
+    for (let artista in data) {
+      data[artista].forEach(musica => {
+        musicas.push(musica);
+      });
+    }
+  });
+
+    console.log(musicas); //teste
+
+//Botão de play
+const btnplay = document.getElementsByClassName("botao-player-tocar")[0];
+
+btnplay.addEventListener("click", () => {
+  if(musica.paused) {
+    musica.play();
+    //muda pra icone pausado aqui
+  }else {
+    musica.pause();
+    //muda pra icone de tocar aqui
+  }
+});
+
+//Botão de avançar e voltar:
+window.addEventListener("load", () => {
+  const btnmudar = document.getElementsByClassName("botao-player-voltaravancar");
+  btnmudar[0].addEventListener("click", () => mudarmusica("-"));
+  btnmudar[1].addEventListener("click", () => mudarmusica("+"));
+
+function mudarmusica(direcao) {
+  if (direcao === "+") {
+    indexmusica = (indexmusica + 1) % musicas.length;
+  } else {
+    indexmusica = (indexmusica - 1 + musicas.length) % musicas.length;
+  }
+}
+})
+
+
+
+/*function mudarmusica(index) {
+  switch(index) {
+        case "+":
+            musicas++;
+            if(musicas == 4) {
+                musicas = 1;
+            }
+            break;
+        case "-":
+            musicas--;
+            if(musicas == 0) {
+                musicas = 3;
+            }
+            break;
+    }
+} */
