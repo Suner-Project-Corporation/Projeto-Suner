fbe5fa31b29ce36b690ee0f7620d141b866d7817
Caio <caio.balassoni1510@gmail.com>
2026-04-03 14:37:22 -0300

    termino da responsividade pagina inicial

--- src/python/criadorJson.py
+++ src/python/criadorJson.py
@@ -45,30 +45,3 @@ with open("musicas.json", "w", encoding="utf-8") as f:
     json.dump(artistas, f, indent=4, ensure_ascii=False)
 
 print("gerado")
-
-# from tinytag import TinyTag
-# from mutagen.id3 import ID3
-# import os
-# pastaMusica = "./src/assets/audio/musicas/"
-# listaMusica = os.listdir(pastaMusica)
-
-# def extrairCapa(musica, nome):
-#     audio = ID3(musica)
-#     imagens = audio.getall("APIC")
-#     if imagens:
-#         dadosImg = imagens[0].data
-#         with open(f"./src/assets/image/musicas/{nome}.jpg", "wb") as f:
-#             f.write(dadosImg)
-
-# def extrairMetadados():
-#     listDados = []
-#     for arquivo in listaMusica:
-#         caminhoMusica = os.path.join(pastaMusica, arquivo)
-#         tag = TinyTag.get(caminhoMusica)
-#         extrairCapa(caminhoMusica, tag.title)
-#         listDados.append([{"tipo":"musica", "nome":tag.title,"artista":tag.artist}])
-#     return listDados
-
-
-# print(extrairMetadados())
-
